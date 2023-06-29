153686f734a6c918c35697f8bf6b3f568a4837a5
AadityaNayak <aadityanayak7@gmail.com>
2023-06-30 01:32:31 +0530

    Added logo, changed navbar theme

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import NavBar from "./NavBar";
+
+function Header(props) {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+Header.propTypes = {};
+
+export default Header;
