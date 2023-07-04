dda59f2ceb9846e7adf67a701c7499246d5762a1
AadityaNayak <aadityanayak7@gmail.com>
2023-07-04 20:49:52 +0530

    Made frontend folder modified gitignore

--- Frontend/src/components/News.jsx
+++ Frontend/src/components/News.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import NewsItem2 from "./NewsItem2";
 import Headers from "./Header";
 import newsItemCss2 from "../styles/newsItem2.css";
 
 const News = (props) => {
+  let [heading, setheading] = useState("");
+  let [articles, setarticles] = useState([]);
 
-    let [heading, setheading] = useState("");
-    let [articles, setarticles] = useState([]);
+  async function load() {
+    let API_KEY = "17d463e1f60c466583ea35a35d19b971";
+    // let API_KEY = "5b3b4ed476cc4feebcf0ddd1e415da42";
+    // let API_KEY = "ff22fa5cb7ac4383aa120e77d9423ef1";
 
-    async function load(){
-  let API_KEY = "17d463e1f60c466583ea35a35d19b971";
-    // let API_KEY = "17";
     let url;
     if (props.category == null) {
-      setheading("Top Headlines")
+      setheading("Top Headlines");
       console.log("null category");
       url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`;
     } else {
@@ -29,19 +30,18 @@ const News = (props) => {
     let parseData = await data.json();
     console.log(parseData);
     setarticles(parseData.articles);
-    }
+  }
 
-    useEffect(() => {
-      load();
-    }, [heading, articles])
-    
+  useEffect(() => {
+    load();
+  }, [heading, articles]);
 
-  return(
+  return (
     <div>
       <h2 className="heading">{heading}</h2>
 
       <div className="news-container">
-      {articles?.map((element, index) => {
+        {articles?.map((element, index) => {
           console.log(element);
           return (
             <NewsItem2
@@ -59,6 +59,6 @@ const News = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default News;
