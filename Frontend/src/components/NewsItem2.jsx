e70c830bb5c9b6cbfff8bd17befcc29862d1cd17
AadityaNayak <aadityanayak7@gmail.com>
2023-07-09 19:24:22 +0530

    Handled null values of author fetching from API

--- Frontend/src/components/NewsItem2.jsx
+++ Frontend/src/components/NewsItem2.jsx
@@ -9,12 +9,12 @@ function NewsItem2(props) {
       <div className="news-item">
         <div className="news-item-sec-1">
           <div className="news-item-title">
-            <h5>{props.title}</h5>
+            <h5>{props.title != null ?props.title: "Unknown"}</h5>
           </div>
           <hr />
-          <div className="news-item-date">Date: {props.date}</div>
+          <div className="news-item-date">Date: {props.date != null ? props.date: "Unknown"}</div>
           <hr />
-          <div className="news-item-source">Source: {props.source}</div>
+          <div className="news-item-source">Source: {props.source != null ?props.source:"Unknown"}</div>
           <hr />
         </div>
         <div className="news-item-sec-2" style={{backgroundImage: `url(${props.imgUrl == null ? "/images/logo.png" : props.imgUrl})`}}>
@@ -22,12 +22,12 @@ function NewsItem2(props) {
         <div className="news-item-sec-3">
             <hr />
           <div className="news-item-description">
-          {props.description}
+          {props.description != null ? props.description : "Unknown"}
           </div>
           <hr />
-          <div className="news-item-author">Author: {props.author}</div>
+          <div className="news-item-author">Author: {props.author != null ? props.author : "Unknown"}</div>
           <div className="nav-item-btn">
-               <a href={`${props.newsUrl}`} target="_blank">View article</a>
+               <a href={`${props.newsUrl != null ? props.newsUrl: "/"}`} target="_blank">View article</a>
           </div>
         </div>
       </div>
